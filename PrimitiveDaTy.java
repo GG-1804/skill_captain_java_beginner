6255a00dadd9ea52161eacf558007d49855a0091
GG-1804 <127873438+GG-1804@users.noreply.github.com>
2023-09-10 20:09:10 +0530

    Create PrimitiveDaTy.java

---
+++ PrimitiveDaTy.java
@@ -0,0 +1,33 @@
+public class PrimitiveDaTy{
+    public static void main(String[] args){
+
+        byte a=18;
+
+        short b=2356;
+
+        int c=789456;
+        
+        long d=895656422L;
+
+        float f=55.6f;
+
+        double g=78.365412;
+
+        boolean h=true;
+        boolean i=(c>1000);
+
+        char j='A';
+
+        String ty="HELLO";
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
